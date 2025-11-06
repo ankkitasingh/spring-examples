999dfd05d2fba83daec0ae659996734a04e56b62
ankita.s <ankkitasingh35@gmail.com>
2025-11-06 23:18:28 +1100

    commit the connection issues while testing

--- api/src/test/java/com/bank/api/CatlogControllerTest.java
+++ api/src/test/java/com/bank/api/CatlogControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,7 +23,7 @@ import com.bank.api.dto.TopSellerView;
 import com.bank.api.model.Product;
 import com.bank.api.services.CatalogService;
 
-
+@ActiveProfiles("test")
 @WebMvcTest(CatalogController.class)
 public class CatlogControllerTest {
 	
--- api/src/test/java/com/bank/api/OrderControllerTest.java
+++ api/src/test/java/com/bank/api/OrderControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +26,7 @@ import com.bank.api.model.User;
 import com.bank.api.services.OrderService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@ActiveProfiles("test")
 @WebMvcTest(OrderController.class)
 public class OrderControllerTest {
 	
